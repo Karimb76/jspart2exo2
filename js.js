a73e411d98b63da7930ce5a9d0b9e240385f5715
karimb76 <karim.bellet@gmail.com>
2018-10-09 12:22:55 +0200

    maj

---
+++ js.js
@@ -0,0 +1,6 @@
+document.getElementById('lastname').addEventListener('focusout', function(){
+var lastname = document.getElementById('lastname').value;
+alert('Merci de votre participation')
+
+}
+)
